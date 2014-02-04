1ecebb03daa54e129a55603dd537dded1a23d690
Sparkst3r <sparkst3r@gmail.com>
2014-02-04 23:09:20 +0000

    Added custom title bar which is draggable

--- src/com/sparkst3r/organised/Organised.java
+++ src/com/sparkst3r/organised/Organised.java
@@ -1,27 +1,48 @@
 package com.sparkst3r.organised;
 
+import java.awt.Color;
 import java.awt.Rectangle;
 
 import javax.swing.SwingUtilities;
 
+import com.extlib.ComponentMover;
 import com.sparkst3r.Library;
 import com.sparkst3r.organised.gui.ContentPanel;
 import com.sparkst3r.window.SparkFrame;
 import com.sparkst3r.window.SparkPanel;
 
+/**
+ * Organised
+ * 
+ * Main class of the program
+ * 
+ * @author Josh Lockheed {Sparkst3r}
+ * @since 4 Feb 2014 00:40:18
+ *
+ */
 public class Organised {
+	public static Rectangle size = new Rectangle(700, 500);
 	
 	public SparkFrame frame = new SparkFrame();
-	public ContentPanel panel = new ContentPanel();
-	public static Rectangle size = new Rectangle(Library.SCREENSIZE.width / 2, Library.SCREENSIZE.height / 2);
+	public ContentPanel panel = new ContentPanel(frame, size);
 	
+	
+	/**
+	 * Constructor, entry point into the program
+	 * @param args Command prompt arguments passed to the program
+	 */
 	public Organised(String[] args) {
-		frame.addElement(panel, new Rectangle(0,0, size.width, size.height));
-		
-		frame.setLayout(null);
 		frame.setBounds((Library.SCREENSIZE.width / 2) - (size.width / 2), (Library.SCREENSIZE.height / 2) - (size.height / 2), size.width, size.height);
+
+		
+		frame.addElement(panel, new Rectangle(0, 0, size.width, size.height));
+		
+		
+		
+
 		frame.setDefaultCloseOperation(SparkFrame.EXIT_ON_CLOSE);
 		frame.setUndecorated(true);
+		frame.setLayout(null);
 		frame.setVisible(true);
 	}
 	
--- src/com/sparkst3r/organised/gui/ContentPanel.java
+++ src/com/sparkst3r/organised/gui/ContentPanel.java
@@ -1,10 +1,50 @@
 package com.sparkst3r.organised.gui;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import com.extlib.ComponentMover;
+import com.sparkst3r.window.SparkFrame;
 import com.sparkst3r.window.SparkPanel;
 
+/**
+ * ContentPanel
+ * 
+ * Contains all elements displayed on the frame
+ * 
+ * @author Josh Lockheed {Sparkst3r}
+ * @since 4 Feb 2014 22:05:18
+ *
+ */
 public class ContentPanel extends SparkPanel {
 
-	public ContentPanel() {
+	public ContentPanel(SparkFrame frame, Rectangle size) {
+		this.setBackground(new Color(255, 255, 255));
+		
+		
+		SparkPanel titleBar = new SparkPanel();
+		titleBar.setBackground(new Color(255, 255, 255));
+		
+		
+		
+		titleBar.addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent e) {
+		    	((SparkPanel) e.getSource()).setBackground(new Color(240, 240, 240));
+		    }
+		    public void mouseExited(MouseEvent e) {
+		    	((SparkPanel) e.getSource()).setBackground(new Color(255, 255, 255));
+		    }
+		} );
+		
+	
+		this.addElement(titleBar, new Rectangle(0, 0, (int)size.getWidth(), 45));
+		new ComponentMover(frame, titleBar);
+		
+		
+		
 		this.setLayout(null);
 	}
 }
